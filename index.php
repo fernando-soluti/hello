61c949fc77dfe67bc7775370a2092039e83b91e2
Fernando Paixao <fernando.paixao@soluti.com.br>
2022-11-13 12:53:12 -0300

    second test

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
-echo "Dude";
+
+require_once __DIR__.'/vendor/autoload.php';
+
+use App\Greeter;
+
+$greeter = new Greeter();
+$output = $greeter->greet("Cody");
+
+var_dump("Dude ",$output);
